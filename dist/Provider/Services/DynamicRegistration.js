4a2895fdaebd61d792eaebca60a3ff877876cbb5
yuvalyellin <yuvalyellin@gmail.com>
2024-11-19 12:29:38 +0200

    Update DynamicRegistration.js

--- dist/Provider/Services/DynamicRegistration.js
+++ dist/Provider/Services/DynamicRegistration.js
@@ -133,7 +133,7 @@ class DynamicRegistration {
     };
     provDynamicRegistrationDebug('Tool registration request:');
     provDynamicRegistrationDebug(registration);
-    provDynamicRegistrationDebug('Sending Tool registration request');
+    provDynamicRegistrationDebug('Sending Tool registration request '+JSON.stringify(registration));
     const body = {
       json: registration,
       headers: registrationToken ? {
@@ -141,6 +141,7 @@ class DynamicRegistration {
       } : undefined
     };
     provDynamicRegistrationDebug('Posting to ' + configuration.registration_endpoint, body);
+    provDynamicRegistrationDebug('Posting2 to ' + configuration.registration_endpoint + JSON.stringify(body));
     const registrationResponse = await got.post(configuration.registration_endpoint, body).json();
 
     // Registering Platform
